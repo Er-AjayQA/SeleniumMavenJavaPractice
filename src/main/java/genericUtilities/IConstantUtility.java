4329f4d0863da0fc0cab87ddd70427abf8250341
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 13:47:49 +0530

    Adding "testData" and "PropertyFile" to project. Also adding path to IConstantUtility interface.

--- src/main/java/genericUtilities/IConstantUtility.java
+++ src/main/java/genericUtilities/IConstantUtility.java
@@ -11,11 +11,11 @@ public interface IConstantUtility {
 		 * This is "Excel File Path".
 		 * This file contains all the test data in it.
 		 */
-		public static String excelFilePath= "";
+		public static String excelFilePath= ".\\src\\test\\resources\\TestData.xlsx";
 		
 		/**
 		 * This is "Property File Path".
 		 * This file contains all the configuration in the form of "Keys-Values" format.
 		 */
-		public static String propertyFilePath= "";
+		public static String propertyFilePath= ".\\src\\test\\resources\\PropertyFile.properties";
 }
