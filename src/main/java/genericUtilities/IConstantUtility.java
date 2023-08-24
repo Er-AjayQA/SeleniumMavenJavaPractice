482d4d025df4fc67fd418c14dc5896b463449f9a
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 13:42:10 +0530

    IConstantUtility interface which holds data like which is constant, like paths etc.

---
+++ src/main/java/genericUtilities/IConstantUtility.java
@@ -0,0 +1,21 @@
+package genericUtilities;
+
+/**
+ * @author Ajay Kumar
+ * This is "IConstantUtility" interface, which holds data like "Paths" which will remains constant for whole project.
+ * This interface will hold the path for "ExcelFile", "PropertyFile", etc.
+ */
+public interface IConstantUtility {
+
+		/**
+		 * This is "Excel File Path".
+		 * This file contains all the test data in it.
+		 */
+		public static String excelFilePath= "";
+		
+		/**
+		 * This is "Property File Path".
+		 * This file contains all the configuration in the form of "Keys-Values" format.
+		 */
+		public static String propertyFilePath= "";
+}
