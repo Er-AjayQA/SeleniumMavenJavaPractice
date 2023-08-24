46af4b5ab367107e0e67771c28aa7fc3aa9d6c5c
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 14:14:47 +0530

    PropertyFileUtility helps to fetch data from property file.

---
+++ src/main/java/genericUtilities/PropertyFileUtility.java
@@ -0,0 +1,26 @@
+package genericUtilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Ajay Kumar
+ * This class contains the property file methods to fetch data.
+ */
+public class PropertyFileUtility {
+
+	/**
+	 * This method fetch and return the desired property value from property file.
+	 * @param property
+	 * @return
+	 * @throws IOException
+	 */
+	public String fetchPropertyFileData(String property) throws IOException
+	{
+		FileInputStream file= new FileInputStream(IConstantUtility.propertyFilePath);
+		Properties pro= new Properties();
+		pro.load(file);
+		return pro.getProperty(property);
+	}
+}
