305e59fe034d9c00edeb96071aa1e0d8e4321005
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 14:42:58 +0530

    Launching browser with using testNG dependency for practice.

---
+++ src/test/java/practicleImplementationOfCodes/LaunchBrowser.java
@@ -0,0 +1,18 @@
+package practicleImplementationOfCodes;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+	
+	@Test
+	public void launchFirefoxBrowser()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
