fa10dd842eaa6714e9e2bea456b9a1997b8393e3
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 21:46:44 +0530

    Adding "Below" locator.

--- src/test/java/locators/BelowAndAboveLocatorsTest.java
+++ src/test/java/locators/BelowAndAboveLocatorsTest.java
@@ -23,5 +23,8 @@ public class BelowAndAboveLocatorsTest{
 		driver.get("https://way2automation.com/way2auto_jquery/index.php");
 		WebElement email = driver.findElement(RelativeLocator.with(By.xpath("//input[@name='email']")).above(By.xpath("//select[@name='country']")));
 		email.sendKeys("abc@gmail.com");
+		
+		WebElement city= driver.findElement(RelativeLocator.with(By.xpath("//input[@name='city']")).below(By.xpath("//select[@name='country']")));
+		city.sendKeys("New Delhi");
 	}
 }
