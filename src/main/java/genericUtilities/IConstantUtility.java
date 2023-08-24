d6767646bbaa4cb6043f1e82a044e0bf66b8719f
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 13:36:37 +0530

    "IConstantUtility" class for all the constant values like "excelFilePath", "propertyFilePath" and etc.

---
+++ src/main/java/genericUtilities/IConstantUtility.java
@@ -0,0 +1,21 @@
+package genericUtilities;
+
+/**
+ * @author Ajay Kumar
+ * This is "IConstantUtility" class, which holds data like "Paths" which will remains constant for whole project.
+ * This class will hold the path for "ExcelFile", "PropertyFile", etc.
+ */
+public class IConstantUtility {
+
+	/**
+	 * This is "Excel File Path".
+	 * This file contains all the test data in it.
+	 */
+	public static String excelFilePath= "";
+	
+	/**
+	 * This is "Property File Path".
+	 * This file contains all the configuration in the form of "Keys-Values" format.
+	 */
+	public static String propertyFilePath= "";
+}
