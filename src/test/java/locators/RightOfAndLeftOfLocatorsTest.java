3f3336c496303e61512e1aa1e3104d31b3747721
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 22:18:50 +0530

    "toRightOf" relative locator example code.

---
+++ src/test/java/locators/RightOfAndLeftOfLocatorsTest.java
@@ -0,0 +1,28 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * This example shows how can we use "toRightOf()" relative locator.
+ */
+public class RightOfAndLeftOfLocatorsTest {
+
+	@Test
+	public void rightOfAndLeftOfLocatorsTest()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver= new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://way2automation.com/way2auto_jquery/index.php");
+		WebElement userName= driver.findElement(RelativeLocator.with(By.xpath("//input[@name='username']")).toRightOf(By.xpath("//label[contains(.,'Username:')]")));
+		userName.sendKeys("AjayCloud");
+		
+		
+	}
+}
