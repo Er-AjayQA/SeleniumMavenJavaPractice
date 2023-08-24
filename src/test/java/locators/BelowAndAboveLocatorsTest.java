2bd575895485514a4d05e16784029ef90cbd9d1b
Ajay Kumar <AuthorName@AjayKumar>
2023-08-24 21:42:19 +0530

    Example of "Above and Below" relative locators.

---
+++ src/test/java/locators/BelowAndAboveLocatorsTest.java
@@ -0,0 +1,27 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * This is an example of "Above and Below" relative locators.
+ */
+
+public class BelowAndAboveLocatorsTest{
+
+	@Test
+	public void belowAboveLocatorTest()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver= new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://way2automation.com/way2auto_jquery/index.php");
+		WebElement email = driver.findElement(RelativeLocator.with(By.xpath("//input[@name='email']")).above(By.xpath("//select[@name='country']")));
+		email.sendKeys("abc@gmail.com");
+	}
+}
