bb398a5e812a33dc56ea3460243b40339db606b0
Ajay Kumar <AuthorName@AjayKumar>
2023-08-26 02:38:11 +0530

    Updating the description.

--- src/test/java/browserLaunching/LaunchFireFoxBrowserTest.java
+++ src/test/java/browserLaunching/LaunchFireFoxBrowserTest.java
@@ -7,16 +7,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * @author Ajay Kumar
- * This class contains method i.e. responsible to launch the "Firefox" browser.
+ * This class contains method i.e. responsible to launch the "FireFox" browser.
  */
 
 public class LaunchFireFoxBrowserTest {
 
+	/**
+	 * This method is for launching the "FireFox" browser.
+	 */
 	@Test
 	public void launchFireFoxBrowserTest()
 	{
-		WebDriverManager.firefoxdriver().setup(); // Setup firefox browser driver.
-		WebDriver driver= new FirefoxDriver(); // launching the firefox browser.
+		WebDriverManager.firefoxdriver().setup(); // Setup "FireFox" browser driver.
+		WebDriver driver= new FirefoxDriver(); // launching the "FireFox" browser.
 		driver.get("https://google.com");
 	}
 }
