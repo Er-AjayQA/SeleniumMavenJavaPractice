3e34501dab2d62f419aa00218454739e889b502e
Ajay Kumar <AuthorName@AjayKumar>
2023-08-26 01:49:48 +0530

    change browser value to chrome.

--- src/test/java/windowsPopupsFrames/AuthenticationPopUpTest.java
+++ src/test/java/windowsPopupsFrames/AuthenticationPopUpTest.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.HasAuthentication;
 import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import genericUtilities.BaseClassUtility;
@@ -31,8 +29,8 @@ public class AuthenticationPopUpTest extends BaseClassUtility{
 	@Test
 	public void authenticationPopUpRegisterMethodTest()
 	{
-		WebDriverManager.edgedriver().setup();
-		WebDriver driver= new EdgeDriver();
+		System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		HasAuthentication hAuth= (HasAuthentication)driver;
 		hAuth.register(UsernameAndPassword.of("admin", "admin"));
 		driver.get("https://the-internet.herokuapp.com/basic_auth");
