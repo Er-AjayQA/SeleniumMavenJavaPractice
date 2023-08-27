76f08417a217de59af39457c1d9b53b81f7aef4e
Ajay Kumar <AuthorName@AjayKumar>
2023-08-27 21:01:19 +0530

    Updating LoginPageTest method.

--- src/test/java/testCases/LoginPageTest.java
+++ src/test/java/testCases/LoginPageTest.java
@@ -5,25 +5,29 @@ import java.io.IOException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import genericUtilities.BaseClassUtility;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPageTest extends BaseClassUtility {
 
 	@Test
 	public void loginPageTest() throws IOException
 	{
-		System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver= new FirefoxDriver();
 		wUtil.maximizeWindow(driver);
 		String url= pUtil.fetchPropertyFileData("TestLoginUrl");
 		driver.get(url);
-		String userID= pUtil.fetchLocatorsFromORFile("username");
-		String passwordID= pUtil.fetchLocatorsFromORFile("password");
-		String submitBtn= pUtil.fetchLocatorsFromORFile("submit");
+		String userID= pUtil.fetchLocatorsFromORFile("UserName_ID");
+		String passwordID= pUtil.fetchLocatorsFromORFile("PassWord_ID");
+		String submitBtn= pUtil.fetchLocatorsFromORFile("SubmitBtn_ID");
+		String userName= pUtil.fetchPropertyFileData("LoginUserName");
+		String passWord= pUtil.fetchPropertyFileData("LoginPassWord");
 		
-		driver.findElement(By.id(userID)).sendKeys("student");
-		driver.findElement(By.id(passwordID)).sendKeys("Password123");
+		driver.findElement(By.id(userID)).sendKeys(userName);
+		driver.findElement(By.id(passwordID)).sendKeys(passWord);
 		driver.findElement(By.id(submitBtn)).click();
 		
 	}
