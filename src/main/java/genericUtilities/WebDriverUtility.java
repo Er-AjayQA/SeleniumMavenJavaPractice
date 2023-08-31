7b1a67193846fb8c23c8aa179bb11f3caea8affe
Ajay Kumar <AuthorName@AjayKumar>
2023-08-31 22:45:04 +0530

    ScreenShot method added to webdriver utility class.

--- src/main/java/genericUtilities/WebDriverUtility.java
+++ src/main/java/genericUtilities/WebDriverUtility.java
@@ -1,8 +1,14 @@
 package genericUtilities;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import com.google.common.io.Files;
 
 public class WebDriverUtility {
 	
@@ -23,6 +29,22 @@ public class WebDriverUtility {
 	public void implicitWait(WebDriver driver, int waitTime)
 	{
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(waitTime));
+	}
+	
+	/**
+	 * This method is for taking screenshot and return path of screenshot.
+	 * @param driver
+	 * @param screenShotName
+	 * @return
+	 * @throws IOException
+	 */
+	public String takeScreenShot(WebDriver driver, String screenShotName) throws IOException
+	{
+		TakesScreenshot sc= (TakesScreenshot)driver;
+		File src= sc.getScreenshotAs(OutputType.FILE);
+		File dest= new File(".\\src\\test\\resources\\Screenshots\\"+screenShotName+".png" );
+		Files.copy(src, dest);
+		return dest.getAbsolutePath();
 	}
 
 }
