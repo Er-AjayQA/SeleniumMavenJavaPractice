3e43b8da4e32c32bbf391991239729b3ca0b4e12
Ajay Kumar <AuthorName@AjayKumar>
2023-09-01 18:43:51 +0530

    generating reportNG.

--- src/test/java/typesOfReports/ReportNGTest.java
+++ src/test/java/typesOfReports/ReportNGTest.java
@@ -1,5 +1,20 @@
 package typesOfReports;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class ReportNGTest {
 
+	@Test
+	public void generatingReportNGTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
 }
